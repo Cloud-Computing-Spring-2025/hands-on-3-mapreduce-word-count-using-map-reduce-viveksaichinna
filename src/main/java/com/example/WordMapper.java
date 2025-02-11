7236d05997e667625e40aba6b4fe2bb2252d4e56
Vivek Sai Chinna Burada <143427585+viveksaichinna@users.noreply.github.com>
2025-02-11 17:34:27 +0000

    final submission

--- src/main/java/com/example/WordMapper.java
+++ src/main/java/com/example/WordMapper.java
@@ -1,5 +1,33 @@
+// package com.example;
+
+// public class WordMapper{
+    
+// }
 package com.example;
 
-public class WordMapper{
-    
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        
+        String line = value.toString();
+        String[] words = line.split("\\s+");  
+
+       
+        for (String w : words) {
+            if (!w.isEmpty()) {  
+                word.set(w.replaceAll("[^a-zA-Z0-9]", "").toLowerCase()); 
+                context.write(word, one);
+            }
+        }
+    }
 }
